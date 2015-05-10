4a39f38e8f3b514695d12f8076ce085fca0588ce
suvendra <suvendra.ee@gmail.com>
2015-05-11 04:43:46 +0530

    backcheck

--- www/js/index.js
+++ www/js/index.js
@@ -17,15 +17,6 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
 		document.addEventListener('deviceready', function() {
 			var exitApp = false, intval = setInterval(function (){exitApp = false;}, 1000);
 			document.addEventListener("backbutton", function (e){
@@ -40,27 +31,6 @@ var app = {
 				} 
 			}, false);
 		}, false);
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
 			$(document).ready(function(){
 				$('a').click(function(e){
 					e.preventDefault();
